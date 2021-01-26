5d7f544d0a5ee44d2429619c7ad30996fb0cdb57
Admin <technicalmaster432@gmail.com>
2021-01-26 23:22:53 +0530

    @data annotation of lombok

--- Exercise-2/src/main/java/com/idd/entity/Product.java
+++ Exercise-2/src/main/java/com/idd/entity/Product.java
@@ -13,13 +13,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@ToString
+
 @Entity
 @Table(name = "product")
-public class Product {
+public @Data class Product {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
